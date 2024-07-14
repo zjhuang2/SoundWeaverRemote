a0c3f843aec08e2e2e53b4b0b8457f22bc3de7ae
Jeremy Huang <jeremy.zq.huang@gmail.com>
2024-07-13 21:36:41 -0400

    Initial Commit

---
+++ SoundWeaverRemote/SoundWeaverRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundWeaverRemoteApp.swift
+//  SoundWeaverRemote
+//
+//  Created by Jeremy Huang on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundWeaverRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
